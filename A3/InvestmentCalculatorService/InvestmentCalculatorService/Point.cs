3d5024c98544ca6902321bcdb2fddd1ac55bfc1f
Jared <hamjared5@gmail.com>
2021-02-20 14:26:35 -0700

    A3 inital commit

---
+++ A3/InvestmentCalculatorService/InvestmentCalculatorService/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InvestmentCalculatorService
+{
+    public class Point
+    {
+        public int x {get; set;}
+        public double y { get; set; }
+
+        Point(int x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
