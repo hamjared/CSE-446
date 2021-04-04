7cc3b606f9997805aa0cb839231c56c792bf227f
Jared <jaredlham@gmail.com>
2021-04-03 20:24:28 -0600

    update

--- New folder/A7/A7/Program.cs
+++ New folder/A7/A7/Program.cs
@@ -31,7 +31,27 @@ namespace A7
             // End Question 1.2 part a
 
             // Question 1.2 part b
+            Console.WriteLine("Question 1.2 Part B:");
 
+            var courseGroups =
+                from c in courses
+                group c by c.Subject into g1
+                select new { Subject = g1.Key,
+                    subGroup = from c in g1 
+                               group c by c.Code into g2
+                               where g2.Count() >= 2
+                               select g2.Key
+                    };
+
+            foreach (var course in courseGroups)
+            {
+                Console.WriteLine("\t" + course.Subject);
+                foreach (var code in course.subGroup)
+                {
+                    Console.WriteLine("\t\t" + code);
+                }
+                
+            }
             // End Questions 1.2 part b
 
             // Question 1.4 part 2
